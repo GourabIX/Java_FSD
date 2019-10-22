7d6a8b08501d493d7ae71bb0b6a7b11a71127ef9
GourabIX <GourabIX@users.noreply.github.com>
2019-10-22 16:59:42 +0530

    Added microservice code

--- Assignment/microservices/currency-conversion-service/src/main/java/com/zensar/currency/conversion/CurrrencyExchangeServiceProxy.java
+++ Assignment/microservices/currency-conversion-service/src/main/java/com/zensar/currency/conversion/CurrrencyExchangeServiceProxy.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 // @FeignClient(name = "forex-service", url = "localhost:8000")
 @FeignClient(name = "forex-service")
-@RibbonClient(name = "forex-service")
+@RibbonClient(name = "forex-service")					// This is a load balancer
 public interface CurrrencyExchangeServiceProxy 
 {
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
